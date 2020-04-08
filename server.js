9a1e77898f9cae4b536f2ae1973213f4b73c970d
Timits <will4f21@yahoo.com>
2020-04-08 05:22:07 -0700

    added npm express port mongoose env

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+require('dotenv').config();
+//console.log(process.env)
+//console.log("Hello World");
+
+// Database
+mongoose.connect('mongodb://127.0.0.1:27017/signup-rest-api', { useNewUrlParser: true })
+// promise
+.then(() => console.log('Connected to database...'))
+.catch(err => console.error(err));
+
+// Middleware
+app.use(express.urlencoded({extended: true }));
+app.use(express.json());
+
+// Controllers
+
+
+// Routes
+
+
+// Start Server
+app.listen(process.env.port || 3000, () => console.log('Server has started on port 3000...'));
+
